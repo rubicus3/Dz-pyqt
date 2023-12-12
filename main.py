ad1d089e0ee2067505288b4a9b38e5705bd05b23
rubicus <laba625@yandex.ru>
2023-12-12 17:23:25 +0300

    Full app logic works

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ class MainWindow(QtWidgets.QWidget, Ui_Form):
         QtWidgets.QWidget.__init__(self, parent)
         self.setupUi(self)
 
+        self.btnClear.clicked.connect(self.clear_input)
+        self.btnSave.clicked.connect(self.save_file)
+        self.btnQuit.clicked.connect(self.quit_app)
+
+    def clear_input(self):
+        self.textEdit.clear()
+
+    def save_file(self):
+        with open("1.txt", "w") as f:
+            f.write(self.textEdit.toPlainText())
+
+    def quit_app(self):
+        QtWidgets.QApplication.quit()
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
