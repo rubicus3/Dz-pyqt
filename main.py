569f4d4de1ce271f2587f7717573981198d946b6
rubicus <laba625@yandex.ru>
2023-12-12 17:15:16 +0300

    Added design to app

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import sys
 
 from PyQt6 import QtWidgets
 
+from window import Ui_Form
+
+
+class MainWindow(QtWidgets.QWidget, Ui_Form):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.setupUi(self)
+
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QWidget()
+    window = MainWindow()
 
     window.show()
     app.exec()
-
